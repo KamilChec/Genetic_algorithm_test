6c08a071315837aa8651e0959ac23a636ab00343
mbrinkiewicz <marcin.brinkiewicz@gmail.com>
2017-01-14 17:51:48 +0100

    Update Animation.java

--- src/genetic_algorithm/Animation.java
+++ src/genetic_algorithm/Animation.java
@@ -2,15 +2,23 @@ package genetic_algorithm;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * Created by dorota on 12.01.17.
  */
 public class Animation extends JPanel {
 
+    private BufferedImage house;
+    private BufferedImage store;
+    private BufferedImage trash;
+    private BufferedImage playground;
     private List<Population> currentPop;
     private Individual currentBest;
     private Population pop;
@@ -32,10 +40,36 @@ public class Animation extends JPanel {
         size = pop.getIndividual(0).getGeneRowLength(0);
         scale=(int)600/size;
         dist=(int)scale/2;
+
+        try {
+            house = ImageIO.read(Animation.class.getResource("/resources/images/H.png"));
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+        try {
+            store = ImageIO.read(Animation.class.getResource("/resources/images/S.png"));
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+        try {
+            trash = ImageIO.read(Animation.class.getResource("/resources/images/T.png"));
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+        try {
+            playground = ImageIO.read(Animation.class.getResource("/resources/images/P.png"));
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+
         toolkit = Toolkit.getDefaultToolkit();
         timer = new Timer();
         timer.schedule(new RemindTask(), 0, 50);
-        this.setBackground(Color.black);
+        this.setBackground(Color.white);
 
     }
 
@@ -47,8 +81,8 @@ public class Animation extends JPanel {
         super.paintComponent(g);
         Graphics2D area = (Graphics2D)g;
         area.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        area.setBackground(Color.black);
-        area.setColor(Color.white);
+        area.setBackground(Color.white);
+        area.setColor(Color.black);
         area.drawRect(dist,dist,size*scale,size*scale);
         Building build;
         pop = currentPop.get(k-1);
@@ -59,22 +93,18 @@ public class Animation extends JPanel {
                 build = pop.getFittest().getGene(i, j).buildingType;
                 switch (build) {
                     case HOUSE:
-                        area.drawRect(X,Y,dist,dist);
-                        area.drawString("H",X+5,Y+15);
+                        area.drawImage(house,X,Y,dist,dist,this);
                         break;
                     case STORE:
-                        area.drawRect(X,Y,dist,dist);
-                        area.drawString("S",X+5,Y+15);
+                        area.drawImage(store,X,Y,dist,dist,this);
                         break;
                     case TRASHCAN:
-                        area.drawRect(X,Y,dist,dist);
-                        area.drawString("T",X+5,Y+15);
+                        area.drawImage(trash,X,Y,dist,dist,this);
                         break;
                     case EMPTY:
                         break;
                     case PLAYGROUND:
-                        area.drawRect(X,Y,dist,dist);
-                        area.drawString("P",X+5,Y+15);
+                        area.drawImage(playground,X,Y,dist,dist,this);
                         break;
                     case TEMP:
                         break;
